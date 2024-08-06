34017f194592e59a3326b81dfe4cdfbc20520370
jqy <jiangqiyu@mail.shareintelli.com>
2024-08-06 10:44:11 +0800

    Initial Commit

---
+++ WxLiveGiftPlayer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WxLiveGiftPlayer
+//
+//  Created by J.qy on 2024/8/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WxLiveGiftPlayer/WxLiveGiftPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WxLiveGiftPlayerApp.swift
+//  WxLiveGiftPlayer
+//
+//  Created by J.qy on 2024/8/6.
+//
+
+import SwiftUI
+
+@main
+struct WxLiveGiftPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
