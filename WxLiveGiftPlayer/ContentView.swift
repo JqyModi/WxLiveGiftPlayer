e66bc6ababdcbd16d3d33c286304c9c945e3335b
jqy <jiangqiyu@mail.shareintelli.com>
2024-08-08 11:00:06 +0800

    feat：移除列表滚动条、特效播放隐藏列表

--- WxLiveGiftPlayer/ContentView.swift
+++ WxLiveGiftPlayer/ContentView.swift
@@ -15,62 +15,60 @@ struct ContentView: View {
     @State var currentPath: String = ListModel.listData().randomElement()?.pagPath ?? ""
     
     @State var pathDidChange = false
+    @State var giftListHidden = false
     
     var body: some View {
         ZStack {
             VStack {
                 Image("live_bg")
                     .imageScale(.large)
-//                    .colorMultiply(Color.gray.opacity(0.9))
-//                    .mask({
-//                        Rectangle()
-//                            .colorMultiply(Color.black.opacity(0.9)) // 应用半透明黑色效果
-//                    })
             }
             .padding()
             
             Rectangle()
-//                .background(Color.red.opacity(0.5))
                 .colorMultiply(Color.black.opacity(0.5)) // 应用半透明黑色效果
             
-            ScrollView {
-                LazyVStack(alignment: .leading, content: {
-                    ForEach(models, id: \.self) { model in
-                        HStack(alignment: .center, spacing: 4) {
-                            Image(model.smallImg)
-                                .resizable()
-                                .frame(width: 24, height: 24)
-                            Text(model.smallTitle)
-                                .fontWeight(.bold)
-                                .foregroundColor(.white)
-                            Text(" > ")
-                                .fontWeight(.bold)
-                                .foregroundColor(.orange)
-                            Image(model.bigImg)
-                                .resizable()
-                                .frame(width: 24, height: 24)
-                            Text(model.bigTitle)
-                                .fontWeight(.bold)
-                                .foregroundColor(.green)
+            if !giftListHidden {
+                ScrollView {
+                    LazyVStack(alignment: .leading, content: {
+                        ForEach(models, id: \.self) { model in
+                            HStack(alignment: .center, spacing: 4) {
+                                Image(model.smallImg)
+                                    .resizable()
+                                    .frame(width: 24, height: 24)
+                                Text(model.smallTitle)
+                                    .fontWeight(.bold)
+                                    .foregroundColor(.white)
+                                Text(" > ")
+                                    .fontWeight(.bold)
+                                    .foregroundColor(.orange)
+                                Image(model.bigImg)
+                                    .resizable()
+                                    .frame(width: 24, height: 24)
+                                Text(model.bigTitle)
+                                    .fontWeight(.bold)
+                                    .foregroundColor(.green)
+                            }
+                            .disabled(false)
+                            .padding(EdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 10))
+                            .onTapGesture {
+                                print("点击", model.pagPath)
+                                currentPath = model.pagPath
+                                print("点击1", $currentPath.wrappedValue)
+                                pathDidChange.toggle()
+                                giftListHidden.toggle()
+                            }
                         }
-                        .disabled(false)
-//                        .background(Color(white: 0.0, opacity: 0.4))
-                        .padding(EdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 10))
-                        .onTapGesture {
-                            print("点击", model.pagPath)
-                            currentPath = model.pagPath
-                            print("点击1", $currentPath.wrappedValue)
-                            pathDidChange.toggle()
-                        }
-                    }
-                })
+                    })
+                }
+                .scrollIndicators(.hidden)
+                .frame(width: 230)
+    //            .background(Color(white: 0.0, opacity: 0.3))
+    //            .mask({
+    //                LinearGradient(gradient: Gradient(colors: [Color.black, Color.black.opacity(0.99) , Color.black.opacity(0.88), Color.black.opacity(0.4)]), startPoint: .leading, endPoint: .trailing)
+    //            })
+                .padding(EdgeInsets(top: 100, leading: 12, bottom: 100, trailing: 170))
             }
-            .frame(width: 230)
-//            .background(Color(white: 0.0, opacity: 0.3))
-//            .mask({
-//                LinearGradient(gradient: Gradient(colors: [Color.black, Color.black.opacity(0.99) , Color.black.opacity(0.88), Color.black.opacity(0.4)]), startPoint: .leading, endPoint: .trailing)
-//            })
-            .padding(EdgeInsets(top: 100, leading: 12, bottom: 100, trailing: 170))
             
             if pathDidChange {
                 PlayPagView(pagPath: $currentPath.wrappedValue)
@@ -79,6 +77,7 @@ struct ContentView: View {
 //                    .disabled(true)
                     .onTapGesture {
                         pathDidChange.toggle()
+                        giftListHidden.toggle()
                     }
             }
         }
